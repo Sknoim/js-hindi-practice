1a14b473a2c406d68755ecd5d6e4fd947a821ec5
Sknoim <sknoim04@gmail.com>
2025-04-23 18:08:55 +0000

    conversion of datatype

---
+++ 01.basics/03_conversion0operation.js
@@ -0,0 +1,25 @@
+//let score = "33"
+//let score = "33abc"
+//let score = "null"
+//let score = true
+//let score = false
+let score = "noim"
+
+//const {score} = req.body
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+//let isLoggedIn = 1
+//let isLoggedIn = 0
+//let isLoggedIn = "noim"
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(someNumber)
+console.log(typeof stringNumber)
