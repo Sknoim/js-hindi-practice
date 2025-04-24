fd1778c47374ab99b4957905bc202346407fcfb0
Sknoim <sknoim04@gmail.com>
2025-04-24 18:00:32 +0000

    conversion

---
+++ 01.basics/04.js
@@ -0,0 +1,24 @@
+let value = 3
+let negative = -value
+//console.log(negative)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2/2)
+//console.log(2**2)
+//console.log(2%3)
+//console.table([2+2, 2-2, 2/2, 2**3, 2%8])
+let str1 = "hello"
+let str2 = " good morning"
+//console.log(str1+str2)
+//console.log("1"+2)
+//console.log(2+"1")
+//console.log("1"+2+2)
+//console.log(1+2+"2")
+//console.log(1+"2"+4)
+let num1,num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+//gameCounter++;
+++gameCounter
+console.log(gameCounter)
